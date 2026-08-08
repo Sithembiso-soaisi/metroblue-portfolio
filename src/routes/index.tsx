14d0bb0964682887b496ca4dac117dbbd9481f19
sithembiso-soaisi <sithembisosoaisi@gmail.com>
2026-08-08 20:01:14 +0200

    Set up project architecture and routing

---
+++ src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Services from "../pages/Services";
+import Projects from "../pages/Projects";
+import About from "../pages/About";
+import Careers from "../pages/Careers";
+import Contact from "../pages/Contact";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/careers" element={<Careers />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
